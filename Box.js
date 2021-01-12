93b33818ee2f3bd9e15d6a7199b21e991bdd2e45
Harshal-Goswami <74553575+Harshal-Goswami@users.noreply.github.com>
2021-01-12 08:52:56 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,30 +1,7 @@
-class Box{
-constructor(x,y, width , height){
-var options ={
-isStatic:false,  
-restitution:0.3,
-friction:0.5,
-density:1.2,
-}
- this.Body = Bodies.rectangle(x,y,width,height);
- this.width = width;
- this.height = height;
+class Box extends BaseClass{
+  constructor(x, y, width, height) {
 
-  World.add(world, this.Body);
- }
-
- display(){
- var pos = this.Body.position;
- var angle = this.Body.angle;
-push();  
-translate(pos.x , pos.y);
-rotate(angle);
-rectMode(CENTER);
-fill(255);
-rect (0 , 0 , this.width , this.height);
-pop();
- }
-
-
-
-}
+  super(x,y , width, height)
+  this.image = loadImage ("sprites/wood1.png")
+  } 
+};
